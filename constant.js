2953e73eaed54dd125e4802e45ab099ca19498cc
Divya1702 <61285597+Divya1702@users.noreply.github.com>
2024-04-05 22:13:26 +0100

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,18 @@
+const browserversion = "Chrome";
+function getBrowserVersion() {
+    if (browserversion == "Chrome") {
+        var browserversion;
+    }
+    console.log(browserversion);
+}
+getBrowserVersion();
+
+const browserversion1 = 'Chrome';
+function getBrowserVersion1() {
+    if (browserversion1 == 'Chrome') {
+        let browserversion;
+    }
+    console.log(browserversion1);
+}
+getBrowserVersion1();
+
